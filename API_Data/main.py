ed69f5d5a672635521ebb1216e772277c0a2d847
theprogrammer36 <theprogrammer36@gmail.com>
2023-09-30 14:18:44 +0530

    Added text blob integration

--- API_Data/main.py
+++ API_Data/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from textblob import TextBlob
 
 app = FastAPI()
+
+class Sentence(BaseModel):
+    sentence: str
+
+
+@app.post('/analyze')
+def analyze(sentenceReference: Sentence):
+    blob = TextBlob(sentenceReference.sentence)
+    return blob.sentiment
 
 
 @app.get('/')
 def intro():
-    return "Hello World"
+    return "Hello Shaik"
